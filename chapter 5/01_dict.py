7d48127ae92fab26283d01a1a93f3689655e6d41
Diya Paliwal <diyapaliwal04.dp@gmail.com>
2025-08-23 23:51:02 +0530

    chapter 5 updated

--- chapter 5/01_dict.py
+++ chapter 5/01_dict.py
@@ -9,5 +9,8 @@ print (marks["Harry"])
 print (marks.items())
 print (marks.keys())
 print (marks.values())
-marks.update({"Harry": 99})
+marks.update({"Harry": 99, "Renuka":100})
 print(marks)
+
+# print(marks.get("Harry2"))   # prints none
+# print(marks["Harry2"])  # returns an error
