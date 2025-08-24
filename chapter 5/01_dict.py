143980480bc0f54e902114ededb005b6b7031ec5
Diya Paliwal <diyapaliwal04.dp@gmail.com>
2025-08-24 23:20:40 +0530

    chapter 5 update

--- chapter 5/01_dict.py
+++ chapter 5/01_dict.py
@@ -6,11 +6,3 @@ marks = {
 
 print(marks, type(marks))
 print (marks["Harry"])
-print (marks.items())
-print (marks.keys())
-print (marks.values())
-marks.update({"Harry": 99, "Renuka":100})
-print(marks)
-
-# print(marks.get("Harry2"))   # prints none
-# print(marks["Harry2"])  # returns an error
---
+++ chapter 5/02_dict_methods.py
@@ -0,0 +1,14 @@
+marks = {
+    "Harry" : 100,
+    "Shubham" : 56,
+    "Rohan" : 23
+}
+
+print (marks.items())
+print (marks.keys())
+print (marks.values())
+marks.update({"Harry": 99, "Renuka":100})
+print(marks)
+
+# print(marks.get("Harry2"))   # prints none
+# print(marks["Harry2"])  # returns an error
