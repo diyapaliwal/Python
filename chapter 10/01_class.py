53915294fb4ad729ab533ded8ebc7c4d2d2fa979
Diya Paliwal <diyapaliwal04.dp@gmail.com>
2025-09-05 21:39:09 +0530

    chapter 10

--- chapter 10/01_class.py
+++ chapter 10/01_class.py
@@ -1,7 +1,16 @@
+# class Employee:
+#     name = "Diya"
+#     language = "Py"
+#     salary = 1200000
+    
+# diya = Employee()
+# print(diya.name, diya.language)
+
+# similarly new one
 class Employee:
-    name = "Diya"
     language = "Py"
     salary = 1200000
     
 diya = Employee()
-print(diya.name, diya.language)
+diya.name = "diya"
+print(diya.name,diya.language, diya.salary)
