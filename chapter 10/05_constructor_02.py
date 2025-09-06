1dd9660fb846f0dfec27813ffb5004493629912e
Diya Paliwal <diyapaliwal04.dp@gmail.com>
2025-09-07 00:13:08 +0530

    chapter 10 update

--- chapter 10/05_constructor_02.py
+++ chapter 10/05_constructor_02.py
@@ -8,6 +8,12 @@ class Employee():
     def __init__(self):  # works without calling
         print("This is a constructor.")
         
+    def __init__(self,name,s,l):
+        self.name = name
+        self.s = s
+        self.l = l
+        print(f"{name} earns in lakhs aprox. {s}rs just by learning {l}")
+        
 manas = Employee()
 manas.name = "Manas Paliwal"
 print(manas.name,manas.l, manas.s)
