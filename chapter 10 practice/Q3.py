a28cb24e75fa47a098f67a0cc1c53b3813759e01
Diya Paliwal <diyapaliwal04.dp@gmail.com>
2025-09-08 01:30:09 +0530

    chapter 10 practice

--- chapter 10 practice/Q3.py
+++ chapter 10 practice/Q3.py
@@ -1,1 +1,9 @@
 # Create a class with a class attribute a; create an object from it and set ‘a’ directly using ‘object.a = 0’. Does this change the class attribute?
+class xyz:
+    a = 123
+        
+obj = xyz()
+obj.a = 0
+print(obj.a)
+
+# yes it changed the class attribute as we all know that instance atrribute is prioritised
