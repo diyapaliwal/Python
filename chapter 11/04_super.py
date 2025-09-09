0dbd6f307215d30ac154a586080b7d3aa4a61df1
Diya Paliwal <diyapaliwal04.dp@gmail.com>
2025-09-09 23:59:34 +0530

    chapter 11 update

---
+++ chapter 11/04_super.py
@@ -0,0 +1,18 @@
+class employee:
+    def __init__(self):
+        print("constructor of employee")
+    a= 1
+    
+class programmer(employee):
+    def __init__(self):
+        print("constructor of programmer")
+    b = 2
+    
+class manager(programmer):
+    def __init__(self):
+        super().__init__()
+        print("constructor of manager")
+    c= 3
+    
+o = manager()
+print(o.a, o.b, o.c)
