1218b22e69470622f9e000ec0e137bddae340f0e
Diya Paliwal <diyapaliwal04.dp@gmail.com>
2025-09-16 10:58:26 +0530

    updates for costomization

---
+++ Python basics/mega project Vanta/main.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+ttsx = pyttsx3.init()
+
+def speak():
+    
+if __name__ == "__main__":
+    pass
