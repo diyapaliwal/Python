3a7f1a73a54ec90a9b1ec4ff4483dc36924a1443
Diya Paliwal <diyapaliwal04.dp@gmail.com>
2025-09-21 21:48:30 +0530

    updated

--- Python basics/chapter 7/02_while_loops.py
+++ Python basics/chapter 7/02_while_loops.py
@@ -3,3 +3,12 @@ i = 1
 while(i<=50):
     print(i)
     i += 1
+    
+# while 2<3:
+    # print("Hello!")
+    
+# here while works infinitely because the conditon 2<3 will be true always so using i and incremnting it by i+= 1 is inportant here
+i = 2
+while i<3:
+    print("Hello!")
+    i+=1
